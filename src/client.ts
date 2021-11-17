f1987b8f4bcd5c6c5d675fcaf2221449ab533a6b
Mauko Maunde <maukoese@gmail.com>
2021-11-18 00:40:45 +0300

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,36 @@
-export type ATConfig = {}
-export type ATSmsPayload = {
-to: string|string[],
-from: string,
-message: string
+export class ATClient {
+  private config: {
+    username: '',
+    apiKey: '',
+    from: ''
+  }
+  private message: string;
+  private phones: array;
+  
+  constructor(config) {
+    this.config = config
+  }
+  
+  public sms(message) {
+  this.message = message
+    return this;
+  }
+  
+  public to(to: string | string[]) {
+    this.phones = to
+    
+    return this;
+  }
+  
+  public async send () {
+    const {data} = await fetch({
+      url: '', body: {
+        to: this.phones,
+        from: this.config.from,
+        message: this.message
+      }
+    })
+    
+    return data
+  }
 }
